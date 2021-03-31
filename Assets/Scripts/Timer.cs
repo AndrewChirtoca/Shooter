42db41807caec71336e1b2702bc453197ef2bec8
Andrei Chirtoca <andrewchirtoca@gmail.com>
2021-03-31 23:36:44 +0300

    simple timer script

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+    public UnityEvent onTimerExpired;
+
+    public void StartTimer(float time)
+    {
+        StartCoroutine(TimerRoutine(time));
+    }
+    
+    private IEnumerator TimerRoutine(float time)
+    {
+        yield return new WaitForSeconds(time);
+        onTimerExpired.Invoke();
+    }
+}
