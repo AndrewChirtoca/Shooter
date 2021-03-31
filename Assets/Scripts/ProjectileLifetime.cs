34e8a323c62c685950951c69c0b706b935e40984
Andrei Chirtoca <andrewchirtoca@gmail.com>
2021-03-31 13:03:53 +0300

    implemented a script that would destroy a projectile instance once a timer expires

---
+++ Assets/Scripts/ProjectileLifetime.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLifetime : MonoBehaviour
+{
+    public float maxLifetime = 10f;
+    
+    private void Start()
+    {
+        Destroy(gameObject, maxLifetime);
+    }
+}
