48488a44a91e21078dd6dfc030d761a770c7dd07
Andrei Chirtoca <andrewchirtoca@gmail.com>
2021-03-31 23:37:20 +0300

    simple RigidbodyLookAtCamera script supposed to be looking at player's cam

---
+++ Assets/Scripts/RigidbodyLookAtCamera.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RigidbodyLookAtCamera : MonoBehaviour
+{
+    public Rigidbody rigidbody;
+    private static Transform mainCamTransform;
+
+    private void OnEnable()
+    {
+        if (mainCamTransform == null)
+        {
+            mainCamTransform = Camera.main.transform;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector3 lookAtDirection = mainCamTransform.position - rigidbody.position;
+        var lookRotation = Quaternion.LookRotation(lookAtDirection);
+        rigidbody.MoveRotation(lookRotation);
+    }
+}
