69e2602fcd843b87c0adc2c81d0423ce8d266169
Andrei Chirtoca <andrewchirtoca@gmail.com>
2021-03-31 13:08:22 +0300

    destructable script featuring a hitpoint amount and fires events once damage is received or the heapoints are depleted

---
+++ Assets/Scripts/Destructible.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Destructible : MonoBehaviour
+{
+    public UnityEvent onDamageReceived;
+    public UnityEvent onObjectDestroyed;
+    
+    [SerializeField]
+    private int _hitPoints;
+
+    public int HitPoints
+    {
+        get
+        {
+            return _hitPoints;
+        }
+        set
+        {
+            if (_hitPoints > value)
+            {
+                onDamageReceived.Invoke();
+            }
+            
+            _hitPoints = value;
+            
+            if (_hitPoints < 0)
+            {
+                onObjectDestroyed.Invoke();
+            }
+        }
+    }
+}
