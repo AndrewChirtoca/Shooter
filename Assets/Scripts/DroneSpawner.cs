d2cfa7da1fafbd9fb97f030a44ef1d172b037ade
Andrei Chirtoca <andrewchirtoca@gmail.com>
2021-03-31 23:33:47 +0300

    DroneSpawner script

---
+++ Assets/Scripts/DroneSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneSpawner : MonoBehaviour
+{
+    public GameObject dronePrefab;
+    public int maxDronesCount;
+    public float spawnTime;
+    public Transform spawnTransform;
+    public List<Transform> spawnerPath;
+
+    private int _currentDronesCount;
+    
+    private void OnEnable()
+    {
+        SpawnRecursive();
+    }
+
+    private void SpawnRecursive()
+    {
+        if (enabled)
+        {
+            if (_currentDronesCount < maxDronesCount)
+            {
+                var droneInstance = Instantiate(dronePrefab, spawnTransform.position, spawnTransform.rotation);
+                droneInstance.GetComponent<PathFollower>().checkPoints = spawnerPath;
+                droneInstance.SetActive(true);
+                _currentDronesCount++;
+            }
+            Invoke(nameof(SpawnRecursive), spawnTime);
+        }
+    }
+}
