4371fdfbe7f78d9f8bbb5c2b277dcfbe5cc39b8e
Andrei Chirtoca <andrewchirtoca@gmail.com>
2021-03-31 13:29:25 +0300

    gun trigger and projectile launcher scripts

---
+++ Assets/Scripts/GunTrigger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GunTrigger : MonoBehaviour
+{
+    public UnityEvent onTriggerPulled;
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            onTriggerPulled.Invoke();
+        }
+    }
+}
---
+++ Assets/Scripts/ProjectileLauncher.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLauncher : MonoBehaviour
+{
+    public Transform launchTransform;
+    public float impulseMagnitude;
+    public Rigidbody projectilePrefab;
+    
+    public void LaunchProjectile()
+    {
+        var projectileInstance = Instantiate(projectilePrefab, launchTransform.position, launchTransform.rotation);
+        projectileInstance.AddForce(launchTransform.forward.normalized * impulseMagnitude, ForceMode.Impulse);
+    }
+}
