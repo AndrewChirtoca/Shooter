f9721278b53f5865ed2a3cf37fef8906dd31766c
Andrei Chirtoca <andrewchirtoca@gmail.com>
2021-03-31 13:29:57 +0300

    SoundClipRandomizer script

---
+++ Assets/Scripts/SoundClipRandomizer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundClipRandomizer : MonoBehaviour
+{
+    public AudioSource audioSource;
+    public List<AudioClip> clips;
+
+    public void PlayRandomClip()
+    {
+        int randomIndex = Random.Range(0, clips.Count);
+        audioSource.clip = clips[randomIndex];
+        audioSource.Play();
+    }
+}
