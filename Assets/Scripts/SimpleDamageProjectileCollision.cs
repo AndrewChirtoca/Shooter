0c0fc7763531dfae05f027fa1b6494160bcc2fd3
Andrei Chirtoca <andrewchirtoca@gmail.com>
2021-03-31 13:25:35 +0300

    SimpleDamageProjectionCollision meant to mimick the most basic damage model applyed on collider contact

---
+++ Assets/Scripts/SimpleDamageProjectileCollision.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleDamageProjectileCollision : MonoBehaviour
+{
+    [SerializeField]
+    private int baseDamage = 20;
+    
+    private void OnCollisionEnter(Collision collision)
+    {
+        var destructable = collision.gameObject.GetComponent<Destructible>();
+        if (destructable != null)
+        {
+            destructable.HitPoints -= baseDamage;
+        }
+    }
+}
