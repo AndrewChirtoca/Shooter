5d35ad3880dbf8779036d7127c1ff0066fcebd89
Andrei Chirtoca <andrewchirtoca@gmail.com>
2021-03-31 23:37:48 +0300

    added the ability for auto fire to the ProjectileLauncher script

--- Assets/Scripts/ProjectileLauncher.cs
+++ Assets/Scripts/ProjectileLauncher.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,7 +8,39 @@ public class ProjectileLauncher : MonoBehaviour
     public Transform launchTransform;
     public float impulseMagnitude;
     public Rigidbody projectilePrefab;
+
+    public bool autoLaunch;
+    public float minAutoFrequency = 4f;
+    public float maxAutoFrequency = 8f;
+
+    private Coroutine _autoLaunchRoutineInstance;
     
+    private void OnEnable()
+    {
+        if (autoLaunch)
+        {
+            _autoLaunchRoutineInstance = StartCoroutine(AutoLaunchRoutine());
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (_autoLaunchRoutineInstance != null)
+        {
+            StopCoroutine(AutoLaunchRoutine());
+        }
+    }
+
+    private IEnumerator AutoLaunchRoutine()
+    {
+        while (true)
+        {
+            float randomTime = UnityEngine.Random.Range(minAutoFrequency, maxAutoFrequency);
+            yield return new WaitForSeconds(randomTime);
+            LaunchProjectile();
+        }
+    }
+
     public void LaunchProjectile()
     {
         var projectileInstance = Instantiate(projectilePrefab, launchTransform.position, launchTransform.rotation);
