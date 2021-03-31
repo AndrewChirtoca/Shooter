15f9f325e0261a7e43901c93b43e2057cb200131
Andrei Chirtoca <andrewchirtoca@gmail.com>
2021-04-01 01:02:18 +0300

    game menu ui script

---
+++ Assets/Scripts/GameMenu.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
+
+public class GameMenu : MonoBehaviour
+{
+    public UnityEvent OnPausePanelShown;
+    public UnityEvent OnPausePanelHidden;
+    private bool _pauseMenuShown = false;
+
+    void OnEnable()
+    {
+        Resume();
+    }
+
+    public void Pause()
+    {
+        _pauseMenuShown = true;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.Confined;
+    }
+
+    public void Resume()
+    {
+        _pauseMenuShown = false;
+        Time.timeScale = 1;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void LoadScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+
+    public void ExitApplication()
+    {
+        Application.Quit();
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false; 
+#endif
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (_pauseMenuShown)
+            {
+                OnPausePanelHidden.Invoke();
+            }
+            else
+            {
+                OnPausePanelShown.Invoke();
+            }
+        }
+    }
+}
